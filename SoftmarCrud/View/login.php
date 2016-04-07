54c60f493f7ca15a3a35a6c6aaed66c6dcb3949f
Andreagzmn <avguzman61@misena.edu.co>
2016-04-07 11:04:32 -0500

    Actualizacion

---
+++ SoftmarCrud/View/login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<head>
+	<meta charset="utf-8">
+	<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">      
+    <link type="text/css" rel="stylesheet" href="materialize/css/materialize.css"  media="screen,projection"/>      
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+  <link rel="stylesheet" type="text/css" href="estilos.css">
+</head>
+</head>
+<body>
+	<div class="con">
+		<div class="tarjeta">		    
+      <div class="row">
+                              </div>
+                          </div>                          
+        		      </form>     
+        		  </div>
+        	 </div>
+      </div>
+    </div>                        
+	</div>
+	  <script type="text/javascript" src="jquery-1.12.1.min.js"></script>
+    <script type="text/javascript" src="materialize/js/materialize.js"></script>
+</body>
+</html>

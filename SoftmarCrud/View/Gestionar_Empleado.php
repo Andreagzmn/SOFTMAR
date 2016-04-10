2d9c65f8660a091f0aa7d397870c0e09efd9d2cf
valentinachch <chicamariavalentina@gmail.com>
2016-04-10 01:02:24 -0500

    boton

--- SoftmarCrud/View/Gestionar_Empleado.php
+++ SoftmarCrud/View/Gestionar_Empleado.php
@@ -1,19 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Formulario</title>
+  <title>Gestionar</title>
   <meta charset="utf-8"/>
    <link type="text/css" rel="stylesheet" href="estilos.css">
-  <title>Gestionar Empleado</title>
+  <title> Gestionar Empleado</title>
   <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <link type="text/css" rel="stylesheet" href="materialize/css/materialize.css"  media="screen,projection"/>
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>  
 </head>
-<body  background="imagenes\fondo.jpg">
-  <section class="contenedor">
-  <img class="logoregis" class="bg-principal contenido col-6-s" src="imagenes\SOFTMAR.png">
-  <H5 id="crea">CREA UNA CUENTA</H5>
-  </section>
+<body  background="img\SOFTMAR.jpg">
+  
   <div class="container">
     <div class="row">
   <form action="../Controller/Empleados.controller.php" method="POST" class="col s12 m8 offset-m2 l6 offset-13 z-depth-4 formulario" >
@@ -63,8 +60,8 @@
       <input type="text" name="Cargo">
       <br>
       
-      <button id="boton" class="btn waves-effect #212121 grey darken-4" type="submit" type="submit" name="accion" value="c">Gestionar</button>
-      <?php echo @$_REQUEST["msn"]; ?>
+      <button type="submit" name="accion" value="c" id="boton" class="btn waves-effect blue darken-3" ><i class=" material-icons right"></i>Gestionar</button>
+                <?php echo @$_REQUEST["msn"]; ?>
     </section>
     
   </form>

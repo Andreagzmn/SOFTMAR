713754b8f245ba7edf914c447b36dc4bd1c53f0e
guillevalen <guivalen@gmail.com>
2016-04-22 15:19:10 -0500

    la misma mierda

--- SoftmarCrud/Controller/empresa.controller.php
+++ SoftmarCrud/Controller/empresa.controller.php
@@ -1,6 +1,6 @@
 <?php
 
-	//incrustar un elemento 1. include: Si no encuentra el elemento nos tira una advertencia 
+	//incrustar un elemento 1. include: Si no encuentra el elemento nos tira una advertencia
 	//y continua ejecutandoel el codigo. uso include("contenido1.php");
 	//2. require: Si no encuentra el elemento nos tira un error y no continua ejecutando el codigo. se usa
 	//require("contenido1.php");
@@ -19,31 +19,31 @@
 		case 'c':
 			#crear...
 			#Inicializar las variables que se envian desde el formulario y las que necesito para almancenar en la tabla.
-			$Cod_TipEmp 	= $_POST["Cod_TipEmp"];			
-			$Nombre			= $_POST["Nombre"];
-			$Telefono		= $_POST["Telefono"];
-			$Direccion      = $_POST["Direccion"];
-			$NIT 			= $_POST["NIT"];
-			$Correo         = $_POST["Correo"];			
+			$Cod_TipEmp 	= $_POST["Cod_TipEmp"];
+			$Nombre				= $_POST["Nombre"];
+			$Telefono			= $_POST["Telefono"];
+			$Direccion    = $_POST["Direccion"];
+			$NIT 					= $_POST["NIT"];
+			$Correo       = $_POST["Correo"];
 			$Informacion	= $_POST["Informacion"];
 			$Dias_aten		= $_POST["Dias_aten"];
 			$Hor_desde		= $_POST["Hor_desde"];
-			$Hor_hasta      = $_POST["Hor_hasta"];
-			$Foto1			= $_POST["Foto1"];
-			$Foto2			= $_POST["Foto2"];
-			$Foto3			= $_POST["Foto3"];
-			$Foto4			= $_POST["Foto4"];
-			$Logo			= $_POST["Logo"];
-			
+			$Hor_hasta    = $_POST["Hor_hasta"];
+			$Foto1				= $_POST["Foto1"];
+			$Foto2				= $_POST["Foto2"];
+			$Foto3				= $_POST["Foto3"];
+			$Foto4				= $_POST["Foto4"];
+			$Logo					= $_POST["Logo"];
+
 			try{
 				Gestion_Empresa::Create($Cod_TipEmp,$Nombre,$Telefono,$Direccion,$NIT,$Correo,$Informacion,$Dias_aten,$Hor_desde,$Hor_hasta,$Foto1,$Foto2,$Foto3,$Foto4,$Logo);
 				$mensaje = "Su registro se creo correctamente";
 				header("Location: ../View/login.php");
 			}catch(Exception $e){
-				$mensaje = "Ha ocurrido un error, el error fue :".$e->getMessage()." en ".$e->getFile()." en la linea ".$e->getLine();	
+				$mensaje = "Ha ocurrido un error, el error fue :".$e->getMessage()." en ".$e->getFile()." en la linea ".$e->getLine();
 				header("Location: ../View/formem.php");
 			}
-			
+
 	break;
 
 
@@ -52,13 +52,13 @@
 		break;
 
 		case 'u':
-			$Cod_Emp 		= $_POST["Cod_Emp"];	
-			$Cod_TipEmp 	= $_POST["Cod_TipEmp"];			
+			$Cod_Emp 		= $_POST["Cod_Emp"];
+			$Cod_TipEmp 	= $_POST["Cod_TipEmp"];
 			$Nombre			= $_POST["Nombre"];
 			$Telefono		= $_POST["Telefono"];
 			$Direccion      = $_POST["Direccion"];
 			$NIT 			= $_POST["NIT"];
-			$Correo         = $_POST["Correo"];			
+			$Correo         = $_POST["Correo"];
 			$Geo_x			= $_POST["Geo_x"];
 			$Geo_y			= $_POST["Geo_y"];
 			$Informacion	= $_POST["Informacion"];
@@ -74,12 +74,12 @@
 				Gestion_Empresa::Update($Cod_Emp,$Cod_TipEmp,$Nombre,$Telefono,$Direccion,$NIT,$Correo,$Geo_x,$Geo_y,$Informacion,$Dias_aten,$Hor_desde,$Hor_hasta,$Foto1,$Foto2,$Foto3,$Foto4,$Logo);
 				$mensaje = "Se actualizo correctamente";
 			}catch(Exception $e){
-				$mensaje = "Ha ocurrido un error, el error fue :".$e->getMessage()." en ".$e->getFile()." en la linea ".$e->getLine();			 
+				$mensaje = "Ha ocurrido un error, el error fue :".$e->getMessage()." en ".$e->getFile()." en la linea ".$e->getLine();
 			}
 			header("Location: ../View/Gestion_Empresa_admin.php?m= ".$mensaje);
 		break;
 
-			
+
 		case 'd':
         try {
           $empresa = Gestion_Empresa::Delete(base64_decode($_REQUEST["ei"]));
@@ -93,7 +93,7 @@
 		default:
 			#hacer cualquier cosa...
 		break;
-	
+
 
 
 	}

4d38238851b9d9e4d6e459f145e618cf8842382b
Rodrigo7M <mtti7@outlook.com>
2016-05-10 10:51:57 -0500

    yap

--- SoftmarCrud/View/comp.menu.php
+++ SoftmarCrud/View/comp.menu.php
@@ -2,7 +2,7 @@
 <link rel="stylesheet" href="../../font-awesome-4.6.2/css/font-awesome.min.css">
 <?php
 if($_SESSION["cod_rol"]==103){
-?>bn
+?>
  
 
  <ul class="right hide-on-med-and-down">
@@ -48,26 +48,3 @@ if($_SESSION["cod_rol"]==103){
 <?php  	   
 }
 ?>
-<footer id="piedepagina" class="page-footer">
-  <div class="container">
-    <div class="row">
-      <div class="col l6 s12">
-        <h5 class="white-text">Footer Content</h5>
-        <p class="grey-text text-lighten-4"></p>
-      </div>
-      <div class="col l4 offset-l2 s12">
-        <h5 class="white-text"></h5>
-          <ul>
-            <li><a id="faceicon"class="grey-text text-lighten-3" href="https://www.facebook.com"><i class="fa fa-facebook-square" aria-hidden="true"></i></a></li>
-            <li><a id="faceicon"class="grey-text text-lighten-3" href="https://www.twitter.com"><i class="fa fa-twitter-square" aria-hidden="true"></i></a></li>
-            <li><a id="faceicon"class="grey-text text-lighten-3" href="https://www.instagram.com/"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
-          </ul>
-      </div>
-    </div>
-  </div>
-  <div id="copy" class="footer-copyright">
-      <div class="container">
-      © Copyright  2016 SOFTMAR todos los derechos reservados
-    </div>
-  </div>
-</footer>

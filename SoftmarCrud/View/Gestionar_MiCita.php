60c17f7d63c615d91177b2dac476ef2985838474
JeisonVC <jvillada50@misena.edu.co>
2016-09-16 09:33:22 -0500

    alertaaa llijjjta

--- SoftmarCrud/View/Gestionar_MiCita.php
+++ SoftmarCrud/View/Gestionar_MiCita.php
@@ -27,37 +27,56 @@
     <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link type="text/css" rel="stylesheet" href="estilos.css">    
     <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Lobster"/>
+   <script>
+      $(document).ready(function(){
+      <?php
+             if(isset($_GET["m"]) and isset($_GET["tm"])){
+               if($_GET["m"] != ""){
+                 echo "
+                            sweetAlert({
+                                 title: 'Mensaje de SOFTMAR',   
+                                 text: '".$_GET["m"]."',   
+                                 type: '".$_GET["tm"]."',   
+                                 showCancelButton: false,
+                                 confirmButtonColor: '#4db6ac',   
+                                 confirmButtonText: 'Aceptar',   
+                                 cancelButtonText: 'No, cancel plx!',   
+                                 closeOnConfirm: false,   
+                                 closeOnCancel: false
+                             });";
+                 }
+               }
+      ?>
 
-<?php
+      $("a#btntrash").click(function(){
 
-       if(isset($_GET["m"]) and isset($_GET["tm"])){
-         if($_GET["m"] != ""){
-           echo "<script>
-                   $(document).ready(function(){
-                      swal({  
-                      title: 'Are you sure?',  
-                      text: '".$_GET["m"]."',   
-                      type: '".$_GET["tm"]."',    
-                      showCancelButton: true,  
-                      confirmButtonColor: '#DD6B55',
-                      confirmButtonText: 'Yes, delete it!', 
-                      closeOnConfirm: false }, 
-                      function(isConfirm){
-                      if (isConfirm){
-                        swal('Deleted!',
-                        'Your imaginary file has been deleted.', 
-                        'success');
-                          }else{
-
-                          }  
-                       
-                         });                  
-                          });
-                </script>";
-           }
-         }
-?>
-
+          var codigo = $("#codcita").val();
+          var accion = "de";
+         sweetAlert({
+                 title: 'Mensaje de SOFTMAR',   
+                 text: 'Esta seguro que desea eliminar el producto?',   
+                 type: 'warning',   
+                 showCancelButton: true,
+                 confirmButtonColor: '#4db6ac',   
+                 confirmButtonText: 'Aceptar',   
+                 cancelButtonText: 'No, cancel!',   
+                 closeOnConfirm: false,   
+                 closeOnCancel: false,
+                 },
+              function(isConfirm){   
+                if (isConfirm) {     
+                    swal("Eliminado!", "se ha eliminado", "success"); 
+                    document.location.href = "../Controller/citas.controller.php?rodri="+codigo+"&c="+accion; 
+                }else{    
+                    swal("Cancelado", "error");   
+                  } 
+      });
+          
+  
+});
+      
+      });
+   </script>
    <link rel="stylesheet" type="text/css" href="Jquery/jquery.dataTables.css">
     <script type="text/javascript" charset="utf8" src="Jquery/jquery.dataTables.js"></script>
 
@@ -102,7 +121,7 @@
       foreach ($citas as $row) {
 
         echo "<tr>
-                <td>".$row["Cod_cita"]."</td>_
+                <td>".$row["Cod_cita"]."</td>
                 <td>".$row["NombreUsu"]."</td>
                 <td>".$row["Telefono"]."</td>
                 <td>".$row["Fecha"]."</td>
@@ -111,11 +130,12 @@
                 <td>".$row["empleado"]."</td>
                 <td>".$row["Nombre"]."</td>
                 <td>
-                  <a href='../Controller/citas.controller.php?rodri=".base64_encode($row["Cod_cita"])."&c=de'><i class='fa fa-trash'></i></a>
+                  <input type='hidden' id='codcita' value='".base64_encode($row["Cod_cita"])."'>
+                  <a href='#' id='btntrash' >
+                  <i class='fa fa-trash'></i></a>
                 </td>
               </tr>";
       }
-
       ?>
       </tbody>
     </table></center>
